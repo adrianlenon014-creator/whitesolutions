c4f137d874473b54e9e6f6fd1b2468862105e527
adrianlenon014-creator <adrianlenon014@gmail.com>
2026-09-17 15:47:30 +0400

    Initial project commit

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const path = require('path');
 const nodemailer = require('nodemailer');
 
 const app = express();
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Whop-Signature, X-Whop-Hmac-Sha256');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  return next();
+});
+
 app.use(express.json({ limit: '1mb' }));
 app.use(express.urlencoded({ extended: true }));
 
